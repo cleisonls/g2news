a67047074b1f396c101b19720b16a1c891f3d093
cleisonls <44264186+cleisonls@users.noreply.github.com>
2018-11-23 12:09:47 -0200

    g2news
    aaa

---
+++ g2news/index1.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<!-- Required meta tags -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.1/css/all.css" integrity="sha384-5sAR7xN1Nv6T6+dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz" crossorigin="anonymous">
+		<link rel="icon" href="iconsenac.ico" type="image/x-icon"/>
+		<link rel="stylesheet" href="css/styleIndex.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+		<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+		<link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+		<title>Fake News</title>
+	</head>
+
+	<body style="background:#cecece">
+		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+		  <div class="collapse navbar-collapse">
+			<ul class="navbar-nav mr-auto">
+				<li class="nav-item">
+					<a href="index1.php"><img src="img/logo4.png" href="index1.php" style="width:45px; margin-left:50px"/></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link active" href="#">&nbsp;&nbsp;&nbsp;&nbsp;Inicio<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">&nbsp;&nbsp;Esporte&nbsp;&nbsp;<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">&nbsp;&nbsp;Economia&nbsp;&nbsp;<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">&nbsp;&nbsp;Política&nbsp;&nbsp;<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">&nbsp;&nbsp;Educação&nbsp;&nbsp;<span class="sr-only">(current)</span></a>
+				</li>
+			</ul>
+		  </div>
+		  <div class="collapse navbar-collapse" >
+			<ul class="navbar-nav mr-auto" >
+				<li class="nav-item" >
+					<a class="nav-link" href="#" style="padding-left:500px">&nbsp;&nbsp;Login&nbsp;&nbsp;<span class="sr-only">(current)</span></a>
+				</li>
+			</ul>
+		  </div>
+		</nav>
+		<div id="topo">
+			<img src="img/fundo.png" style="width:100%;"/>
+		</div>
+		<div class="container-fluid" style="color:#fff; background:#343a40;">
+			<div class="row">
+				<div class="col-md-12">
+					<h2 class="text-center">G2 News</h2>
+				</div>
+			</div>
+		</div>
+		
+		<div class="container" style="margin-top:50px;">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="card" style="width: 17rem;">
+					  <img class="card-img-top" src="img/esporte.jpg" alt="Imagem de capa do card" style="height:250px;">
+					  <div class="card-body">
+						<h5 class="card-title">Esporte</h5>
+						<p class="card-text">Neymar é o artilheiro isolado do Campeonato Francês, com sete gols, o que reprenta 25% dos 27 já marcados pelo arrasador Paris Saint-Germain nas oito...</p>
+						
+						<center><a href="#" class="btn btn-danger">Visitar</a></center>
+					  </div>
+					</div>			
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 17rem;">
+					  <img class="card-img-top" src="img/economia.jpg" alt="Imagem de capa do card" style="height:250px;">
+					  <div class="card-body">
+						<h5 class="card-title">Economia</h5>
+						<p class="card-text">A economia brasileira deverá crescer de 2,5% a 3% em 2019, segundo projeções feitas pelo Departamento Econômico da Associação Nacional das...</p>
+						<center><a href="#" class="btn btn-danger">Visitar</a></center>
+					  </div>
+					</div>			
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 17rem;">
+					  <img class="card-img-top" src="img/politica.jpg" alt="Imagem de capa do card" style="height:250px;">
+					  <div class="card-body">
+						<h5 class="card-title">Política</h5>
+						<p class="card-text">À medida que anuncia os futuros ministros, o presidente eleito, Jair Bolsonaro, emite sinais determinantes dos rumos que pretende dar...</p>
+						<center><a href="#" class="btn btn-danger">Visitar</a></center>
+					  </div>
+					</div>			
+				</div>
+				<div class="col-md-3">
+					<div class="card" style="width: 17rem;">
+					  <img class="card-img-top" src="img/educacao.jpg" alt="Imagem de capa do card" style="height:250px;">
+					  <div class="card-body">
+						<h5 class="card-title">Educação</h5>
+						<p class="card-text">A professora de geologia Adriana Alves, paulistana de 38 anos, acaba de ter a segunda filha. A professora de química Anna Maria "Anita" Canavarro...</p>
+						<center><a href="#" class="btn btn-danger">Visitar</a></center>
+					  </div>
+					</div>			
+				</div>
+			</div>
+		</div>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-md-12">&nbsp;<br><br></div>
+		</div>		
+	</div>
+	
+	<footer class="container-fluid" id="rodape" style="height: 120px;font-family: Montserrat,-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,'Helvetica Neue',Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol','Noto Color Emoji'; background:#343a40; color:#fff">
+			<div class="row">
+				<div class="col-md-2" style="padding-top:22px; padding-left: 80px;">
+					<img src="img/logo4.png" height="60px"/>
+				</div>
+				<div class="col-md-2"  style="padding-top:17px;margin-top:10px;">
+					<span class="nav-link js-scroll-trigger" href="index.html"><i class="fas fa-phone"></i>&nbsp;&nbsp;(21)&nbsp;4002-8922</span>
+				</div>
+				<div class="col-md-4" style="margin-bottom:-10px;margin-top:10px;">
+					<span style="font-size:0.9em">COPYRIGHT © 2018 Todos os direitos reservados.<br>G2 NEWS Comércio digital LTDA. - CNPJ: 11.200.418/0004-01 - R. Cel. Carlos Matos, 86, CENTRO, CEP: 95742-086, Nova iguaçu, RJ, BRASIL.</span>
+				</div>
+				<div class="col-md-2"  style="padding-top:17px;margin-top:10px;">
+					<span class="nav-link js-scroll-trigger" href="index.html"><i class="fab fa-instagram"></i>&nbsp;&nbsp;@g2news</span>
+				</div>
+				<div class="col-md-2"  style="padding-top:17px;margin-top:10px;">
+					<span class="nav-link js-scroll-trigger" href="index.html"><i class="fab fa-facebook"></i>&nbsp;&nbsp;/g2news.com</span>
+				</div>
+			</div>
+	</footer>
+	</body>
+</html>
